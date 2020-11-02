510170eb711ae17d46bac3998e07a3fbe2321a01
robyn <65057146+robynsdev@users.noreply.github.com>
2020-11-02 15:11:08 +1100

    added edit and destroy links and actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,45 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+  end  
+
+  def show
+    @post = Post.find(params[:id])
   end
-  
   
   def new
     @post = Post.new
-  end
+  end  
+
+  def edit
+    @post = Post.find(params[:id]) 
+  end  
 
   def create
     @post = Post.new(post_params)
     
     if @post.save
-    redirect_to @post
+    redirect_to @post  
     else
       render 'new'
-    end
-  end
+    end  
+  end  
   
-  def show
+  def update
     @post = Post.find(params[:id])
+ 
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end  
+  end  
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+ 
+    redirect_to posts_path
   end
 
   private
