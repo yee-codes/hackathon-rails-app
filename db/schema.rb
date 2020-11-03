8700b0dc41a0ba78231f56d9dc78625170ca4506
robyn <65057146+robynsdev@users.noreply.github.com>
2020-11-03 14:21:19 +1100

    added user FK to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_090628) do
+ActiveRecord::Schema.define(version: 2020_11_03_023407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2020_11_02_090628) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,4 +47,5 @@ ActiveRecord::Schema.define(version: 2020_11_02_090628) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "posts", "users"
 end
