d9edc2aba7fce777e5b89f5b0fcb9a07fc749012
yee-codes <shaoyee.ng@gmail.com>
2020-11-04 09:42:00 +1100

    fix show page bug

--- Gemfile
+++ Gemfile
@@ -59,4 +59,3 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "aws-sdk-s3", "~> 1.83"
 
 gem "stripe", "~> 5.28"
-
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -8,3 +8,6 @@ Rails.configuration.stripe = {
 #   Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
   Stripe.api_key = Rails.application.credentials.stripe[:secret_key]
+
+
+  # make sure stripe works
