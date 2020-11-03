ac394e47613d4b1a327cd5f02a5b0c9353150a46
yee-codes <shaoyee.ng@gmail.com>
2020-11-03 21:44:26 +1100

    added payment processing

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,10 @@
+# Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+
+Rails.configuration.stripe = {
+    :publishable_key => ENV['PUBLISHABLE_KEY'],
+    :secret_key      => ENV['SECRET_KEY']
+  }
+  
+#   Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+  Stripe.api_key = Rails.application.credentials.stripe[:secret_key]
